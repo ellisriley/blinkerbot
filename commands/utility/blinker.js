2fe310f070955b2ea8f8aa72d21bed4adf9cab5d
Riley Ellis <riley@ellisnet.me@>
2026-04-14 16:41:04 +0000

    get nickname from tools.getMemberDisplayNameFromId for consistency

--- commands/utility/blinker.js
+++ commands/utility/blinker.js
@@ -1,19 +1,20 @@
 const { SlashCommandBuilder } = require('discord.js');
 const db = require("../../db.js");
-
+const tools = require("../../tools.js");
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('blinker').setDescription('log a blinker with blinkerbot !!'),
 	async execute(interaction) {
 		const userId = interaction.user.id;
 		const record = await db.query("SELECT score FROM scores WHERE userId=\""+userId+"\"");
+		const nickname = await tools.getMemberDisplayNameFromId(interaction, userId);
 		//console.log(record);
 		if (record.length===0) {
 			await db.query("INSERT INTO scores (userId, score) VALUES (\""+userId+"\",1);");
-			await interaction.reply("New competetor! "+interaction.user.displayName+ " has joined");
+			await interaction.reply("New competetor! "+nickname+ " has joined");
 		} else {
 			await db.query("UPDATE scores SET score = score +1 WHERE userId= \""+userId+"\";");
-			await interaction.reply(interaction.user.displayName +"'s blinkers: "+(record[0].score+1));
+			await interaction.reply(nickname +" just hit a blinker! Current score: "+(record[0].score+1));
 		}
 	},
 };
--- commands/utility/leaderboard.js
+++ commands/utility/leaderboard.js
@@ -2,24 +2,6 @@ const tools = require("../../tools.js");
 const { SlashCommandBuilder } = require('discord.js');
 const db = require("../../db.js");
 
-async function getMemberDisplayNameFromId(interaction, userId) {
-    if (!interaction.guild) {
-        console.log("No guild");
-        return undefined;
-    }
-  
-    let member = await interaction.guild.members.cache.get(userId);
-    if (!member) {
-      try {
-        member = await interaction.guild.members.fetch(userId);
-        
-      } catch {
-        return "Unknown";
-      }
-    }
-    
-    return member?.displayName ?? member?.user?.username;
-}
 
 module.exports = {
     data: new SlashCommandBuilder().setName('leaderboard').setDescription('view the blinker leaderboard !!'),
@@ -27,7 +9,7 @@ module.exports = {
         const records = await db.query("SELECT * FROM scores ORDER BY score DESC LIMIT 3;");
         let response = "";
         for (record in records) {
-            let displayName = await getMemberDisplayNameFromId(interaction, records[record].userId);
+            let displayName = await tools.getMemberDisplayNameFromId(interaction, records[record].userId);
             response = response + record + ". " + displayName + ": " + records[record].score + " blinkers\n"; 
         }
         await interaction.reply(response);
