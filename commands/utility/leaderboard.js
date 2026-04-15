efe1af32c2db8b125cd8ad397a7986f070e82227
Riley Ellis <riley@ellisnet.me>
2026-04-15 22:26:24 +0000

    feature: leaderboard command option to select leaderboards for different types

--- commands/utility/leaderboard.js
+++ commands/utility/leaderboard.js
@@ -4,13 +4,26 @@ const db = require("../../db.js");
 
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('leaderboard').setDescription('view the blinker leaderboard !!'),
+    data: new SlashCommandBuilder().setName('leaderboard').setDescription('view the blinker leaderboard !!').addStringOption((option) =>
+			option
+				.setName('category')
+				.setDescription('type of weed to show the leaderboard for')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'blinkers', value: 'blinkers' },
+					{ name: 'joints', value: 'joints' },
+					{ name: 'bongs', value: 'bongs' },
+                    { name: 'edibles', value: 'edibles' },
+				),
+		),
     async execute(interaction) {
-        const records = await db.query("SELECT * FROM scores ORDER BY score DESC LIMIT 6;");
+        const category = interaction.options.getString("category");
+        const records = await db.query("SELECT * FROM scores ORDER BY " + category +" DESC LIMIT 6;");
+        console.log(records);
         let response = "";
         for (record in records) {
             let displayName = await tools.getMemberDisplayNameFromId(interaction, records[record].userId);
-            response = response + record + ". " + displayName + ": " + records[record].blinkers + " blinkers\n"; 
+            response = response + record + ". " + displayName + ": " + records[record][category] + " " + category +"\n"; 
         }
         await interaction.reply(response);
     },
