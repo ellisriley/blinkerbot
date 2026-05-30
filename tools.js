e48b9ef7089a69d5512569ce4afac973b22b485c
Riley Ellis <Riley@ellisnet.me>
2026-05-30 14:47:14 +0000

    feat: generate response with math to generate milestone responses

--- tools.js
+++ tools.js
@@ -1,4 +1,5 @@
 const { GuildMember, CommandInteraction} = require('discord.js');
+const db = require("./db.js");
 
 const logTypes = {
   blinker:"blinkers",
@@ -27,5 +28,31 @@ async function getMemberDisplayNameFromId(interaction, userId) {
     return member?.displayName ?? member?.user?.username;
 }
 
+async function generateResponse(category, amount, nickname) {
+  let responseString = "";
+  if ((amount%25) ===0) {
+    let responses = await db.getMilestoneResponses();
+    return responses[0].response;
+  }else {
+    switch (category){
+      case (logTypes.blinker):
+        responseString = nickname + " just hit a blinker! Current blinkers: " + amount;
+        break;
+      case (logTypes.joint):
+        responseString = nickname + " just smoked a j! Current j's: " + amount;
+        break;
+      case (logTypes.edible):
+        responseString = nickname + " just hit scranned an edible! Current edibles: " + amount;
+        break;
+      case (logTypes.bongs):
+        responseString = nickname + " just hit smoked a bong! Current edibles: " + amount;
+        break;
+      case (logTypes.vape):
+        responseString = nickname + " just hit hit a vape! Current bowls: " + amount;
+        break;
+    }
+  }
+  return responseString;
+}
 
-module.exports = {getMemberDisplayNameFromId, logTypes}
+module.exports = {getMemberDisplayNameFromId, logTypes, generateResponse}
