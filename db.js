ee9e565d238c70fe048614d4340b6c365e01b420
Riley Ellis <Riley@ellisnet.me>
2026-06-27 14:23:03 +0000

    add get current amount (needs completing) fix createUserRecord fix circular dependency for tools seperate checkUserSafe for reuse

--- db.js
+++ db.js
@@ -23,21 +23,38 @@ async function query(sql){
   return res;
 }
 
+async function getTypes() {
+  const res = query(`SELCT * FROM `+dbconf.schema+`.types`);
+  return res.rows;
+}
+
 async function doesUserExist(discordId) {
-  const res = await query(`SELECT FROM `+dbconf.schema+`.users WHERE user_discord_id=`+discordId+`;`);
-  return res.rows.length > 0;
+  const res = await query(`SELECT * FROM `+dbconf.schema+`.users WHERE user_discord_id=`+discordId+`;`);
+  return res.rowCount > 0;
 }
 
 async function createUserRecord(interaction, discordId) {
-  const res = await query(`INSERT INTO `+dbconf.schema+`.users(username, user_discord_id) VALUES ('`+await tools.getMemberDisplayNameFromId(interaction, discordId)+`',`+discordId+`);`);
+  const displayName =await tools.getMemberDisplayNameFromId(interaction, discordId);
+  const res = await query(`INSERT INTO `+dbconf.schema+`.users(username, user_discord_id) VALUES ('`+displayName+`',`+discordId+`);`);
   return res; 
+}
+
+async function checkUserSafe(interaction, userId) {
+  if (await doesUserExist(userId) === false) {
+    await createUserRecord(interaction, userId);
+  } 
 }
 
 async function createLogRecord(interaction, userId, logType){
   const logTime = Math.floor(new Date().getTime() / 1000);
-  if (!doesUserExist(userId)) {createUserRecord(interaction, userId);console.log("creating...");}
+  await checkUserSafe(interaction, userId);
+  console.log(logType);
   const resCreate = await query(`INSERT INTO `+dbconf.schema+`.logs(user_id, log_time, log_type) VALUES ((SELECT user_id FROM `+dbconf.schema+`.users WHERE user_discord_id=`+userId+`), `+logTime+`, (SELECT type_id FROM `+dbconf.schema+`.types WHERE type_description='`+logType+`'));`);
   return resCreate;
+}
+
+async function getCurrentAmount(userId, type) {
+  const value = await query(`SELECT * FROM `+dbconf.schema+`.logs WHERE user_id=(SELECT user_id FROM `+dbconf.schema+`.users WHERE user_discord_id=`+userId+`) AND log_type=(SELECT type_id FROM `+dbconf.schema+`.types WHERE type_description=`+type+`);`);
 }
 
 async function getRecordByUserId(userId) {
