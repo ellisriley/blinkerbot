55e912b32ce82fc2b9dbaff3c4c7cb41efa4d459
Riley Ellis <Riley@ellisnet.me>
2026-06-27 10:30:05 +0000

    change: rewrite db.js for postgres

--- db.js
+++ db.js
@@ -1,47 +1,26 @@
-const mysql = require('mysql2/promise');
+// db.js
+// ver 2.0.0A1
+// riley ellis 2026
+// connects to postgres database and queries for data , running on sql02.pve01.ellisnet.me (postgres01.ellisnet.me)
 
-const db = require("./db.json")
+const pg = require("pg");
 
-async function query(sql, params) {
-  const connection = await mysql.createConnection(db);
-  const [results, ] = await connection.execute(sql, params);
+const pool = new pg.Pool({
+  user: "riley",
+  host: "postgres01.ellisnet.me",
+  database: "blinky",
+  password: "blinky123",
+  port: 5432,
+});
 
-  return results;
+async function query(){
+  const client = await pool.connect();
+  const res = await client.query(`SELECT * FROM blinkystaging.types`);
+  return res;
 }
-
 async function getRecordByUserId(userId) {
-  return await query("SELECT * FROM scores WHERE userId=\""+userId+"\";");
-}
+  const res = await query();
+  return res.rows[0].type_description;
+} 
 
-async function doesRecordExist(userId) {
-  const record = await query("SELECT userId FROM scores WHERE userId=\""+userId+"\";");
-  if (record.length===0) {
-    return false;
-  }else {
-    return true;
-  }
-}
-
-async function createRecord(userId) {
-  await query("INSERT INTO scores (userId, blinkers, joints, edibles, bongs) VALUES (\""+userId+"\",0,0,0,0);");
-}
-
-async function increment(userId, field) {
-  query("UPDATE scores SET " + field + "=" + field + "+1 WHERE userId= \""+userId+"\";");
-}
-
-async function incrementByHalf(userId, field) {
-  query("UPDATE scores SET " + field + "=" + field + "+0.5 WHERE userId= \""+userId+"\";");
-}
-
-async function ammend(userId, field, amount) {
-  query("UPDATE scores SET " + field + "=" + field + "+"+amount+" WHERE userId= \""+userId+"\";");
-}
-
-async function getMilestoneResponses() {
-  return await query("SELECT * FROM responses;");
-}
-
-module.exports = {
-  query, getRecordByUserId, doesRecordExist, createRecord, increment, incrementByHalf, ammend, getMilestoneResponses
-}
+module.exports = { query , getRecordByUserId } 
